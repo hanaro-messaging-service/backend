ad15f55f7b5c3312016866ab37020b04c8c1b562
“songy00n” <allnu@naver.com>
2023-06-23 11:08:44 +0900

    Feat: 보이스피싱 예방 안내 메시지 구현

---
+++ out/artifacts/untitled3_war_exploded/pages/sendMessage/voicefishingNotificationMessage/voicefishingNotificationMessage.js
@@ -0,0 +1,62 @@
+xs
+var debounceTimer;
+function debounce(func, delay) {
+    clearTimeout(debounceTimer); // 타이머를 초기화
+
+    debounceTimer = setTimeout(func, delay); // 일정 시간이 지난 후에 함수를 실행
+};
+function sendValueToServlet() {
+    var selectedNameValue = document.getElementById('name').value;
+    var selectedCheckBoxPrivacyYes = document.getElementById("privacyYes");
+    var selectedAgeValue = document.getElementById("age").value;
+    let sentence = "";
+    console.log(selectedNameValue);
+    sentence += selectedCheckBoxPrivacyYes.checked ? "&selectedPrivacyYesValue=" + encodeURIComponent("Y") : "";
+    sentence += selectedAgeValue !== "전체" ? "&selectedAgeValue=" + encodeURIComponent(selectedAgeValue) : "";
+    sentence += selectedNameValue ? "&selectedNameValue=" + encodeURIComponent(selectedNameValue) : "";
+    // AJAX 요청을 사용하여 서블릿에 값 전달
+
+    debounce(function(){
+        var xhr = new XMLHttpRequest();
+        xhr.open("POST", "/VoiceNotificationServlet", true);
+        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                var parser = new DOMParser();
+                var responseDoc = parser.parseFromString(xhr.responseText, "text/html");
+                var elementValue = responseDoc.getElementById("resultContainer").innerHTML;
+// 모든 <input> 요소를 활성화
+                var inputElements = document.getElementsByTagName("input");
+                for (var i = 0; i < inputElements.length; i++) {
+                    inputElements[i].disabled = false;
+                }
+
+// 모든 <select> 요소를 활성화
+                var selectElements = document.getElementsByTagName("select");
+                for (var i = 0; i < selectElements.length; i++) {
+                    selectElements[i].disabled = false;
+                }
+
+                // 가져온 값으로 특정 영역 업데이트
+                document.getElementById("resultContainer").innerHTML = elementValue;
+            }
+        };
+
+        // 전송할 데이터를 조합하여 한 번에 전송
+        var requestData = "selectedTotalValue=" + encodeURIComponent("total")+sentence;
+// 모든 <input> 요소를 비활성화
+        var inputElements = document.getElementsByTagName("input");
+        for (var i = 0; i < inputElements.length; i++) {
+            inputElements[i].disabled = true;
+        }
+
+// 모든 <select> 요소를 비활성화
+        var selectElements = document.getElementsByTagName("select");
+        for (var i = 0; i < selectElements.length; i++) {
+            selectElements[i].disabled = true;
+        }
+
+        xhr.send(requestData);
+    },1000);
+
+}
